b04af3d4b34866ec79e2f8f405c47239966b3aee
jasper <lylin108@163.com>
2025-08-08 20:59:55 +0800

    build from homebrew.ubuntu22

--- cpp_docker/entrypoint.sh
+++ cpp_docker/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 # 允许生成无限大小的 core dump 文件
 ulimit -c unlimited
 
-# 设置 core dump 文件生成在当前工作目录，文件名格式 core.<exe>.<pid>
-sudo sh -c "echo '/faker/core.%e.%p' > /proc/sys/kernel/core_pattern"
+# # 设置 core dump 文件生成在当前工作目录，文件名格式 core.<exe>.<pid>
+# sudo sh -c "echo './core.%e.%p' > /proc/sys/kernel/core_pattern"
 
 echo "Core pattern set to:"
 cat /proc/sys/kernel/core_pattern
